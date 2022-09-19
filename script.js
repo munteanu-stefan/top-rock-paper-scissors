caedaa2e8b4b4a5723f6daa9c2ced1b3c3df45cb
Munteanu Stefan <Munteanustefan20132@gmail.com>
2022-09-19 20:34:48 +0300

    Add computerChoice() function
    The function returns either 'rock', 'paper' or 'scissors'

---
+++ script.js
@@ -0,0 +1,20 @@
+function computerChoice() {
+    choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0:
+            console.log('Computer picked rock');
+            return 'rock';
+            break;
+        case 1:
+            console.log('Computer picked paper');
+            return 'paper';
+            break;
+        case 2:
+            console.log('Computer picked scissors');
+            return 'scissors';
+            break;
+        default:
+            console.log('Something is wrong');
+            break;
+    }
+}
