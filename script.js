a056b50ef577a3d21e42cfba30ee2ae20340477a
Munteanu Stefan <Munteanustefan20132@gmail.com>
2022-09-19 21:02:06 +0300

    Add compareChoices() and playRound()

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function computerChoice() {
-    choice = Math.floor(Math.random() * 3);
+    const choice = Math.floor(Math.random() * 3);
     switch (choice) {
         case 0:
             console.log('Computer picked rock');
@@ -20,8 +20,33 @@ function computerChoice() {
 }
 
 function playerChoice() {
-    player = prompt('Choose rock, paper or scissors').toLowerCase();
+    const player = prompt('Choose rock, paper or scissors').toLowerCase();
     return player;
 }
 
-playerChoice();
+function compareChoices(player, computer) {
+    if (player === 'rock' || player === 'paper' || player === 'scissors') {
+        if (player === computer) {
+            return `Draw! You both chose ${player}!`;
+        } else if ((player === 'rock' && computer === 'scissors') || 
+                    (player === 'paper' && computer === 'rock') || 
+                    (player === 'scissors' && computer === 'paper')) {
+            return `You won! You chose ${player} and computer chose ${computer}`;
+        } else {
+            return `You lost! You chose ${player} and computer chose ${computer}`
+        }
+    } else {
+        return `Your choice is invalid, ${player} is not an option.`
+    }
+}
+
+
+function playRound() {
+    let player = playerChoice();
+    let computer = computerChoice();
+    let outcome = compareChoices(player, computer);
+    
+    document.getElementById("p1").innerText = outcome;
+}
+
+playRound();
